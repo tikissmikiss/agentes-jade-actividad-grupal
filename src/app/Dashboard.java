4de58b950f627e8d8d36503608d2d16076c25745
Tikissmikiss <josehercepreciado@hotmail.com>
2022-04-22 15:20:22 +0200

    Preliminar Jose

--- src/app/Dashboard.java
+++ src/app/Dashboard.java
@@ -40,11 +40,6 @@ public class Dashboard extends Agent {
     // logging
     private Logger logger = Logger.getMyLogger(this.getClass().getName());
 
-    public static void main(String[] args) {
-        Dashboard app = new Dashboard();
-        app.runApp();
-    }
-
     private static void setSingleton(Dashboard app) {
         unicaInstancia = app;
     }
